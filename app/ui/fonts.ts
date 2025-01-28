7331d95ad4df64ec353de1ac2759088fbc8e91b8
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-01-27 21:24:53 -0300

    fontes e imagens

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import styles from '@/app/ui/home.module.css';
+import { Inter,Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({subsets: ['latin'],weight: ['400', '700'],}) 
+
+
