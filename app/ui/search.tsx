f6653cd7ee1d248b360b9d26a922d564c9bb8ca5
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-01-29 14:00:02 -0300

    exibindo tabela

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -18,6 +18,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     else{
       params.delete('query')
     }
+    
     replace(`${pathName}?${params.toString()}`); // substitui na url
   }
 
