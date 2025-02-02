a91b6c43996b257accb742dd42692335ad48b080
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-02-02 17:57:16 -0300

    validação de formulário

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,29 +3,55 @@ import { custom, z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { AdjustmentsVerticalIcon } from '@heroicons/react/24/outline';
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'}) // conn
 
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pendente','pago']),
+    customerId: z.string({
+      invalid_type_error:'Por favor selecione o cliente'
+    }),
+    amount: z.coerce.number().gt(0, {message: 'Por favor insira um valor maior que R$0'}),
+    status: z.enum(['pendente','pago'],{
+      invalid_type_error:"Por faovr selecione um status."
+    }),
     date: z.string()
 })
+
+export type State ={
+  errors?: {
+    customerId?: string[]
+    amount?: string[]
+    status?: string[]
+  }
+  message?:string | null
+}
 
 const CreateInvoice = FormSchema.omit({id:true, date:true})
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData:FormData) {
-    const {customerId,amount,status} = CreateInvoice.parse( {
+export async function createInvoice(prevState: State,formData:FormData) {
+
+    const validatedFields = CreateInvoice.safeParse( {
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
     });
 
-    const amountInCents = amount *100
+    // console.log(validatedFields.error?.flatten().fieldErrors);
+
+    if(!validatedFields.success){
+      return{
+        errors:validatedFields.error.flatten().fieldErrors,
+        message: "falha ao cria uma nova fatura. Campos ausentes!"
+      }
+    }
+
+    // dado extraidos
+    const {customerId, amount, status} = validatedFields.data;
+    const amountInCents = amount *100;
     const date = new Date().toISOString().split('T')[0];
 
     try {
@@ -69,7 +95,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id:string) {
-  throw new Error('Failed to Delete Invoice');
   await sql `DELETE FROM invoices WHERE id = ${id}`
   
   revalidatePath('/dashboard/invoices');
